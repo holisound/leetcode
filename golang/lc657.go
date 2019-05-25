a5edd2e2cb9769778da6e15b9342b0bc6617b224
edward <edwardw@qq.com>
2019-05-25 21:09:05 +0800

    lc1002,lc520,lc657

---
+++ golang/lc657.go
@@ -0,0 +1,16 @@
+func judgeCircle(moves string) bool {
+	/*
+		这题我学到了：
+		初步形成解决坐标位移类问题的算法框架
+	*/
+	index := map[rune]int{
+		'U': 0, 'D': 1, 'L': 2, 'R': 3,
+	}
+	dx := []int{0, 0, -1, 1}
+	dy := []int{1, -1, 0, 0}
+	x, y := 0, 0
+	for _, m := range moves {
+		x, y = x+dx[index[m]], y+dy[index[m]]
+	}
+	return x == 0 && y == 0
+}
