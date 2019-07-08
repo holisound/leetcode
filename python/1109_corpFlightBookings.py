6ef84337b83cd9386191d67e0c50788f638d342f
Bigcorn <edwardw@qq.com>
2019-07-08 16:15:17 +0800

    Update 1109_corpFlightBookings.py

--- python/1109_corpFlightBookings.py
+++ python/1109_corpFlightBookings.py
@@ -3,12 +3,11 @@ class Solution:
         # 为了帮助理解，在二维坐标轴里，先把线段画出来
         # 线段重叠部分数值累加，
         # 到达线段出发点start，加上数，
-        # 到达线段终点end，同样加上数，到达end+1,减去数
+        # 到达end+1,减去数
 
         count = collections.defaultdict(int)
         for s, e, num in bookings:
             count[s] += num
-            count[e] + num
             count[e + 1] -= num
 
         res = []
