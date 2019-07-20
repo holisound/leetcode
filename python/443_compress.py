12b64c5e4a6c5e770ee0d47a7c0a8450c1479574
edward <edwardw@qq.com>
2019-07-20 17:27:38 +0800

    Create 443_compress.py

---
+++ python/443_compress.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def compress(self, chars):
+        """
+        :type chars: List[str]
+        :rtype: int
+        """
+        j, k = 0, 0
+        N = len(chars)
+        for i in range(1, N + 1):
+            if i == N or chars[j] != chars[i]:
+                chars[k] = chars[j]
+                if i - j > 1:
+                    for x in str(i - j):
+                        k += 1
+                        chars[k] = x
+                k += 1
+
+                j = i
+        return k
