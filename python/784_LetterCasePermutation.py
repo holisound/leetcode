6304820dd0325f3a4559537b514ee03ac8bef1e8
Bigcorn <edwardw@qq.com>
2019-09-27 18:10:50 +0800

    Update 784_LetterCasePermutation.py

--- python/784_LetterCasePermutation.py
+++ python/784_LetterCasePermutation.py
@@ -12,3 +12,18 @@ class Solution:
 
             return res
         return backtrack(S, 0, [S])
+
+        def backtrack2(p, res, n):
+            if p < n:
+                k=-1
+                for i in range(len(res)):
+                    x=res[i]
+                    for j in range(p, n):
+                        if x[j].isalpha():
+                            nx=x[:j]+chr(ord(x[j])^32)+x[j+1:]
+                            res.append(nx)
+                            k=j+1
+                            break
+                if k !=-1:
+                    backtrack2(k, res, n)
+            return res
