d1ce3a82014c2dd70bbc97e6f387bf1bfe263d2c
Bigcorn <edwardw@qq.com>
2019-09-27 21:16:14 +0800

    Update 784_LetterCasePermutation.py

--- python/784_LetterCasePermutation.py
+++ python/784_LetterCasePermutation.py
@@ -8,22 +8,24 @@ class Solution:
                     nc=chr(ord(S[i])^32)
                     ns=S[:i]+nc+S[i+1:]
                     # 注意:下次起始位置应该是i+1, 不是start+1
-                    res.extend(backtrack(ns, i+1, [ns])) 
+                    res.append(ns)
+                    backtrack(ns, i+1, res)
 
             return res
         return backtrack(S, 0, [S])
-
+    
         def backtrack2(p, res, n):
-            if p < n:
-                k=-1
-                for i in range(len(res)):
-                    x=res[i]
-                    for j in range(p, n):
-                        if x[j].isalpha():
-                            nx=x[:j]+chr(ord(x[j])^32)+x[j+1:]
-                            res.append(nx)
-                            k=j+1
-                            break
-                if k !=-1:
-                    backtrack2(k, res, n)
+            k=-1
+            for i in range(len(res)):
+                x=res[i]
+                for j in range(p, n):
+                    if x[j].isalpha():
+                        nx=x[:j]+chr(ord(x[j])^32)+x[j+1:]
+                        res.append(nx)
+                        k=j+1
+                        break
+            if k !=-1:
+                backtrack2(k, res, n)
             return res
+            
+        return backtrack2(0,[S], len(S))
