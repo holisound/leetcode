abfa188cf2558e09a3b4219726a39c6d41323395
Bigcorn <edwardw@qq.com>
2019-09-29 14:37:54 +0800

    Update 22_generateParenthesis.py

--- python/22_generateParenthesis.py
+++ python/22_generateParenthesis.py
@@ -9,3 +9,17 @@ class Solution:
             backtrack(res, l, r + 1, s + ')')
             return res
         return backtrack([], 0, 0, "")
+    
+        #使用queue迭代
+        que=collections.deque([("", 0, 0)])
+        res=[]
+        while que:
+            s,l,r=que.popleft()
+            if l > n or r > l:
+                continue
+            if l==r==n:
+                res.append(s)
+            que.append((s+"(",l+1,r))
+            que.append((s+")",l,r+1))
+            
+        return res
