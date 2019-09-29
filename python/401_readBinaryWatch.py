d063bafd8768d926783b3a70ecd5486fc8df329d
Bigcorn <edwardw@qq.com>
2019-09-29 14:17:24 +0800

    Update 401_readBinaryWatch.py

--- python/401_readBinaryWatch.py
+++ python/401_readBinaryWatch.py
@@ -15,3 +15,22 @@ class Solution:
                 backtrack(digs + 1, count, last, res)
             return res
         return backtrack(0, 0, 0, [])
+
+        # 迭代版本
+        que=collections.deque()
+        que.append((0,0,0))
+        res = []
+        while que:
+            last, cnt, depth = que.popleft()
+            if depth > 10:
+                if cnt==num:
+                    h = last >> 6
+                    m = last - (h << 6)
+                    if 0 <= h < 12 and 0 <= m < 60:
+                        res.append("%d:%02d" % (h, m))
+            else:
+                last <<= 1
+                que.append((last|1, cnt+1, depth+1))
+                que.append((last, cnt, depth+1))
+
+        return res
