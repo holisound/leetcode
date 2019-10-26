fd939140911da4f0439467a20c151365470201e8
Bigcorn <edwardw@qq.com>
2019-10-26 20:14:07 +0800

    Update 424_LongestRepeatingCharacterReplacement_M.py

--- python/424_LongestRepeatingCharacterReplacement_M.py
+++ python/424_LongestRepeatingCharacterReplacement_M.py
@@ -10,3 +10,13 @@ class Solution:
                 j += 1
             res = max(res, i - j + 1)
         return res
+    
+    def characterReplacement2(self, s, k):
+        maxf = res = 0
+        count = collections.Counter()
+        for i in range(len(s)):
+            count[s[i]] += 1
+            maxf = max(maxf, count[s[i]])
+            if res - maxf < k: res += 1
+            else: count[s[i - res]] -= 1
+        return res
