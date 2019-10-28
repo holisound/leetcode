20a188bdd30e4db2011c16d3cf98676f6d806112
Bigcorn <edwardw@qq.com>
2019-10-28 17:22:36 +0800

    Create 833_findReplaceString_M.py

---
+++ python/833_findReplaceString_M.py
@@ -0,0 +1,22 @@
+class Solution:
+    def findReplaceString(self, S: str, indexes: List[int], sources: List[str], targets: List[str]) -> str:
+        pos = {index: i for i, index in enumerate(indexes)}
+        indexes.sort()
+        last = 0
+        m = {}
+        for index in indexes + [len(S)]:
+            m[last] = S[last:index]
+            last = index
+
+        for index in indexes:
+            i = pos[index]
+            temp = m[index]
+            s, t = sources[i], targets[i]
+            if temp.startswith(s):
+                chars = list(temp)
+                chars[:len(s)] = list(t)
+                m[index] = ''.join(chars)
+        res = ''
+        for k in sorted(m):
+            res += m[k]
+        return res
