59ccbdecc225939f14d13fbea76fe9a181552ca6
Bigcorn <edwardw@qq.com>
2019-11-09 11:32:27 +0800

    Update 187_findRepeatedDnaSequences.py

--- python/187_findRepeatedDnaSequences.py
+++ python/187_findRepeatedDnaSequences.py
@@ -1,10 +1,9 @@
 class Solution:
     def findRepeatedDnaSequences(self, s: str) -> List[str]:
-        d={}
+        res, seen=set(),set()
         for i in range(len(s)-10+1):
             win = s[i:i+10]
-            if win in d:
-                d[win]=True
-            else:
-                d[win]=False
-        return [k for k,v in d.items() if v]
+            if win in seen:
+                res.add(win)
+            seen.add(win)
+        return list(res)
