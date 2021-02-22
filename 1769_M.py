b0d604d5234fa0c70f4d670fac27aecc72a664e5
Bigcorn <edwardw@qq.com>
2021-02-22 13:52:47 +0800

    Create 1769_M.py

---
+++ 1769_M.py
@@ -0,0 +1,16 @@
+class Solution:
+    def minOperations(self, boxes: str) -> List[int]:
+        ans = []
+        left = right = total = 0
+        n = len(boxes)
+        for i in range(n):
+            if boxes[i] == '1':
+                right += 1
+                total += i
+        for i in range(n):
+            ans.append(total)
+            if boxes[i] == '1':
+                left += 1
+                right -= 1
+            total += left - right
+        return ans
