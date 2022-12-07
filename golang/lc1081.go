eb91b4b8640fe08b53dd4241a917445b18d291d8
Bigcorn <edwardw@qq.com>
2022-12-07 16:20:20 +0800

    Update lc1081.go

--- golang/lc1081.go
+++ golang/lc1081.go
@@ -3,19 +3,18 @@ https://leetcode.cn/problems/smallest-subsequence-of-distinct-characters/
 1081. 不同字符的最小子序列
 */
 func smallestSubsequence(s string) string {
-    inStack:=map[int]bool{}
     st := &Stack{}
     for i, c := range s {
         k:=int(c)
-        if inStack[k] {
+        if st.Has(k) {
             continue
         }
-        for st.Size()>0 && st.Peek()>k && strings.IndexRune(s[i+1:], rune(st.Peek()))!=-1{
-            inStack[st.Pop()]=false
+        for st.Size()>0 && st.Peek()>k && 
+            strings.IndexRune(s[i+1:], rune(st.Peek()))!=-1{
+            st.Pop()
         }
 
         st.Push(k)
-        inStack[k]=true
     }
     sb:=&strings.Builder{}
     for st.Size()>0 {
@@ -34,22 +33,32 @@ func reverseString(s string)string{
 // 栈, 将int作为中间数据类型，在rune或byte之间转换
 type Stack struct {
     data    []int
+    inStack map[int]int
     sizeCur int
     sizeMax int
 }
 func (s *Stack) Push(x int){
     s.sizeCur++
+    if s.inStack == nil {
+        s.inStack = make(map[int]int)
+    }
     if s.sizeCur > s.sizeMax {
         s.data = append(s.data, x)
         s.sizeMax = s.sizeCur
+        
     } else {
         s.data[s.sizeCur-1] = x
     }
+    s.inStack[x]++
 }
 func (s *Stack) Pop() int{
     res := s.data[s.sizeCur-1]
     s.sizeCur--
+    s.inStack[res]--
     return res
+}
+func (s *Stack) Has(x int) bool {
+    return s.inStack[x]>0
 }
 func (s *Stack) Size() int {
     return s.sizeCur
