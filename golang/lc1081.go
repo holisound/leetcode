f5958645eda206b7e9d2afc7f4f085ff2c67fa21
Bigcorn <edwardw@qq.com>
2022-12-07 16:11:00 +0800

    Update lc1081.go

--- golang/lc1081.go
+++ golang/lc1081.go
@@ -1,34 +1,59 @@
-func smallestSubsequence(text string) string {
-    /*
-       这题我学到了：
-        第一次遍历，先对字符计数
-        第二次遍历，按顺序依次减少剩余字符的数量，同时利用Stack弹出非最优解
-    */
-    stack := make([]rune, len(text))
-    endIndex := -1
-    seen := map[rune]bool{}
-    count := map[rune]int{}
-    for _, c := range text {
-        count[c]++
-    }
-    for _, c := range text {
-        count[c]--
-        if seen[c] {
+/*
+https://leetcode.cn/problems/smallest-subsequence-of-distinct-characters/
+1081. 不同字符的最小子序列
+*/
+func smallestSubsequence(s string) string {
+    inStack:=map[int]bool{}
+    st := &Stack{}
+    for i, c := range s {
+        k:=int(c)
+        if inStack[k] {
             continue
         }
-        for endIndex > -1 {
-            v := stack[endIndex]
-            if v > c && count[v] > 0 {
-                endIndex--
-                delete(seen, v)
-            } else {
-                break
-            }
+        for st.Size()>0 && st.Peek()>k && strings.IndexRune(s[i+1:], rune(st.Peek()))!=-1{
+            inStack[st.Pop()]=false
         }
-        endIndex++
-        seen[c] = true
-        stack[endIndex] = c
-    }
-    return string(stack[:endIndex+1])
 
+        st.Push(k)
+        inStack[k]=true
+    }
+    sb:=&strings.Builder{}
+    for st.Size()>0 {
+        sb.WriteRune(rune(st.Pop()))
+    }
+    return reverseString(sb.String())
+}
+
+func reverseString(s string)string{
+    chars:=[]byte(s)
+    for i,j:=0,len(s)-1;i<j;i,j=i+1,j-1{
+        chars[i],chars[j]=chars[j],chars[i]
+    }
+    return string(chars)
+}
+// 栈, 将int作为中间数据类型，在rune或byte之间转换
+type Stack struct {
+    data    []int
+    sizeCur int
+    sizeMax int
+}
+func (s *Stack) Push(x int){
+    s.sizeCur++
+    if s.sizeCur > s.sizeMax {
+        s.data = append(s.data, x)
+        s.sizeMax = s.sizeCur
+    } else {
+        s.data[s.sizeCur-1] = x
+    }
+}
+func (s *Stack) Pop() int{
+    res := s.data[s.sizeCur-1]
+    s.sizeCur--
+    return res
+}
+func (s *Stack) Size() int {
+    return s.sizeCur
+}
+func (s *Stack) Peek() int{
+    return s.data[s.sizeCur-1]
 }
