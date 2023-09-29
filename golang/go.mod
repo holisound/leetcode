54f982f8b5b9e1eb404c2cd0b1d928d81065f16a
Edward Wang <edwardw@qq.com>
2023-09-29 11:28:12 +0800

    Update lc0394.go

--- golang/go.mod
+++ golang/go.mod
@@ -1,3 +1,5 @@
 module github.com/holisound/leetcode
 
 go 1.19
+
+require github.com/emirpasic/gods v1.18.1 // indirect
--- golang/lc0394/lc0394.go
+++ golang/lc0394/lc0394.go
@@ -3,6 +3,7 @@ package main
 import (
 	"unicode"
 
+	lls "github.com/emirpasic/gods/stacks/linkedliststack"
 	"github.com/holisound/leetcode/utils"
 )
 
@@ -36,6 +37,39 @@ func decodeString(s string) string {
 	res := ""
 	for stack.Size() != 0 {
 		res = string(rune(stack.Pop())) + res
+	}
+	return res
+}
+
+func decodeStringV2(s string) string {
+	stack := lls.New()
+	var chars []rune
+	for _, c := range s {
+		if c == ']' {
+			for val, ok := stack.Peek(); ok && unicode.IsLetter(val.(rune)); val, ok = stack.Peek() {
+				chars = append(chars, val.(rune))
+				stack.Pop()
+			}
+			stack.Pop()
+			base, num := 1, 0
+			for val, ok := stack.Peek(); ok && unicode.IsDigit(val.(rune)); val, ok = stack.Peek() {
+				num, base = num+base*(int(val.(rune)-'0')), base*10
+				stack.Pop()
+			}
+			for i := 0; i < num; i++ {
+				for j := len(chars) - 1; j > -1; j-- {
+					stack.Push(chars[j])
+				}
+			}
+			chars = []rune{}
+			continue
+		}
+		stack.Push(c)
+	}
+	res := ""
+	for val, ok := stack.Peek(); ok; val, ok = stack.Peek() {
+		res = string(val.(rune)) + res
+		stack.Pop()
 	}
 	return res
 }
