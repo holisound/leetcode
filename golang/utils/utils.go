1a460591a55b6dce197692734fe566afabce5423
Edward Wang <edwardw@qq.com>
2023-10-03 10:55:07 +0800

    Add BFSUtil

--- golang/utils/utils.go
+++ golang/utils/utils.go
@@ -46,3 +46,64 @@ func ManhattanDist(x1, y1, x2, y2 int) int {
 	}
 	return x2 - x1 + y2 - y1
 }
+
+var (
+	dx [4]int = [4]int{0, 0, 1, -1}
+	dy [4]int = [4]int{1, -1, 0, 0}
+)
+
+type Pos struct {
+	R, C, Value int
+}
+type BFSUtil struct {
+	nrow, ncol int
+	visited    [][]int
+	Matrix     any
+}
+
+func NewBFSUtil(matrix any) BFSUtil {
+	var nrow, ncol int
+	if _, ok := matrix.([][]int); ok {
+		nrow, ncol = len(matrix.([][]int)), len(matrix.([][]int)[0])
+	} else if _, ok := matrix.([][]byte); ok {
+		nrow, ncol = len(matrix.([][]byte)), len(matrix.([][]byte)[0])
+	}
+	visited := make([][]int, nrow)
+	for i := range visited {
+		visited[i] = make([]int, ncol)
+	}
+	return BFSUtil{nrow, ncol, visited, matrix}
+
+}
+
+func (bfu *BFSUtil) IsGoodPos(pos *Pos) bool {
+	return pos.R > -1 && pos.R < bfu.nrow && pos.C > -1 && pos.C < bfu.ncol
+}
+func (bfu *BFSUtil) IsFirstVisited(pos *Pos) bool {
+	return bfu.visited[pos.R][pos.C] == 1
+}
+func (bfu *BFSUtil) ValidateAddons(pos *Pos) bool {
+	// 自定义方法
+	return true
+}
+func (bfu *BFSUtil) Validate(pos *Pos) bool {
+	return bfu.IsFirstVisited(pos) && bfu.ValidateAddons(pos)
+}
+func (bfu *BFSUtil) SetValueInt(pos *Pos, value int) {
+	mtx := bfu.Matrix.([][]int)
+	mtx[pos.R][pos.C] = value
+}
+func (bfu *BFSUtil) SetValueByte(pos *Pos, value byte) {
+	mtx := bfu.Matrix.([][]byte)
+	mtx[pos.R][pos.C] = value
+}
+func (bfu *BFSUtil) GetNeighbours(pos *Pos) (res []*Pos) {
+	for i := 0; i < 4; i++ {
+		newPos := &Pos{pos.R + dx[i], pos.C + dy[i], pos.Value + 1}
+		if bfu.IsGoodPos(newPos) {
+			bfu.visited[newPos.R][newPos.C] += 1
+			res = append(res, newPos)
+		}
+	}
+	return
+}
